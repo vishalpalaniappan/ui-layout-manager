94f785df697e8d5f79772c83eaef1f7fdb937edf
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-10 16:55:52 -0400

    Fix: Use correct main and module for cjs and esm builds. Point to cjs path directly instead of using package.json file.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,13 +4,12 @@ import terser from '@rollup/plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import { babel } from '@rollup/plugin-babel';
-import packageJson from "./package.json" assert { type: "json" };
 
 export default {
     input: 'src/index.js',
     output: [
         {
-            file: packageJson.module,
+            file: 'dist/cjs/index.js',
             format: 'cjs',
             sourcemap: true
         },
