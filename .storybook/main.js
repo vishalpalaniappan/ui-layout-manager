4f80341fa1420f661152348fdbb8d26b4920a9d0
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-10 00:21:37 -0400

    Updates

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss",
+  ],
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  }
+};
+export default config;
