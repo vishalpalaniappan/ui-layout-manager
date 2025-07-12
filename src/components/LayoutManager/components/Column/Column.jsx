7d37b4a34d0849747b7686d186c90eebdb63ea01
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-12 15:55:37 -0400

    Add row and column components.

---
+++ src/components/LayoutManager/components/Column/Column.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+
+import "./Column.scss";
+
+/**
+ * Renders a column.
+ * 
+ */
+export const Column = ({}) => {
+    return (
+        <></>
+    );
+}
+
+Column.propTypes = {
+}
---
+++ src/components/LayoutManager/components/Row/Row.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+
+import "./Row.scss";
+
+/**
+ * Renders a row.
+ * 
+ */
+export const Row = ({}) => {
+    return (
+        <></>
+    );
+}
+
+Row.propTypes = {
+}
