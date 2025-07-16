357467d5defcceab09f39b2be53324f8e90cfed6
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-15 20:09:45 -0400

    Add context to store component registry.

---
+++ src/components/LayoutManager/Providers/ComponentRegistryContext.js
@@ -0,0 +1,6 @@
+import {createContext} from "react";
+
+// This context stores the registry for the components.
+const ComponentRegistryContext = createContext();
+
+export default ComponentRegistryContext;
