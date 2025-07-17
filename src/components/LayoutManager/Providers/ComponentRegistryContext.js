8d1caaac71f416e9b42570084d5d9b43a1a7bda2
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-17 15:54:12 -0400

    Initialize context with empty object and set display name for react dev tools.

--- src/components/LayoutManager/Providers/ComponentRegistryContext.js
+++ src/components/LayoutManager/Providers/ComponentRegistryContext.js
@@ -1,6 +1,7 @@
 import {createContext} from "react";
 
 // This context stores the registry for the components.
-const ComponentRegistryContext = createContext();
+const ComponentRegistryContext = createContext({});
+ComponentRegistryContext.displayName = 'ComponentRegistryContext';
 
 export default ComponentRegistryContext;
