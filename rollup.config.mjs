56fccfe952cb7aa217e2a0931089e85b5f0e71c5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-28 19:37:22 -0400

    Update rollup build to bundle worker separately.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,31 +5,41 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import { babel } from '@rollup/plugin-babel';
 
-export default {
-    input: 'src/index.js',
-    output: [
-        {
-            file: 'dist/cjs/index.js',
-            format: 'cjs',
-            sourcemap: true
-        },
-        {
-            file: 'dist/esm/index.js',
+export default [
+    {
+        input: 'src/index.js',
+        output: [
+            {
+                file: 'dist/cjs/index.js',
+                format: 'cjs',
+                sourcemap: true
+            },
+            {
+                file: 'dist/esm/index.js',
+                format: 'esm',
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            external(['react', 'react-dom']),
+            resolve({
+                extensions: ['.js', '.jsx'],
+            }),
+            postcss(),
+            terser(),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+            }),
+            commonjs(),
+        ]
+    },
+    {
+        input: 'src/components/LayoutManager/Controller/LayoutWorker.js',
+        output: {
+            file: 'dist/esm/LayoutWorker.js',
             format: 'esm',
-            sourcemap: true,
         },
-    ],
-    plugins: [
-        external(['react', 'react-dom']),
-        resolve({
-            extensions: ['.js', '.jsx'],
-        }),
-        postcss(),
-        terser(),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-        }),
-        commonjs(),
-    ]
-}
+        plugins: [],
+    }
+]
