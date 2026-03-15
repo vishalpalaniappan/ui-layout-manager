a67522b82e6b890856090aefc57c51e17ae8e196
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-15 13:57:22 -0400

    Add layout cacher class.

---
+++ src/components/LayoutManager/Controller/Worker/LayoutCacher.js
@@ -0,0 +1,29 @@
+/**
+ * This class handles all layout caching functionality. It enables the program to 
+ * save the layout to local storage and load it from local storage.
+ */
+
+export class LayoutCacher {
+
+    /**
+     * 
+     * @param {Object} layout 
+     */
+    constructor (layout) {
+
+    }
+
+    /**
+     * Saves the layout state to local storage.
+     */
+    saveToCache () {
+
+    }
+
+    /**
+     * Gets the layout state from local storage.
+     */
+    getFromCache() {
+
+    }
+}
