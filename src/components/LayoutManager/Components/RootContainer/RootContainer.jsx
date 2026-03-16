9c5b592ed64828428c17943458a4e3cafc107830
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-16 05:49:16 -0400

    Move drag preview into its own file.

--- src/components/LayoutManager/Components/RootContainer/RootContainer.jsx
+++ src/components/LayoutManager/Components/RootContainer/RootContainer.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Container } from "../Container/Container";
 import { HandleBar } from "../HandleBar/HandleBar";
 import { useLayoutController } from "../../Providers/LayoutProvider";
+import { DragPreview, offsetOverlay } from "./DragPreview";
 import {
     DndContext,
     DragOverlay,
@@ -15,38 +16,6 @@ import {
 
 import "./RootContainer.scss"
 
-
-/**
- * Preview for the div being dragged.
- * @returns 
- */
-function DragPreview({ label }) {
-    return (
-        <div
-        style={{
-            padding: "6px 12px",
-            background: "#2d2d2d",
-            color: "white",
-            boxShadow: "0 4px 12px rgba(0,0,0,0.3)",
-            opacity:0.3
-        }}
-        >
-        {label}
-        </div>
-    );
-}
-
-/**
- * Offset for the drag overlay.
- * @returns 
- */
-const offsetOverlay = ({ transform }) => {
-  return {
-    ...transform,
-    x: transform.x + 20,
-    y: transform.y + 20
-  };
-};
 
 /**
  * Root node for the layout tree. This component will start 
