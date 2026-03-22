cb388d00e79c5b8a52d09e7dbd83702a2fc52cb0
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-22 11:28:10 -0400

    Randomly pick three files to load for demo ui.

--- src/stories/sample_components/fileeditor/FileEditor.jsx
+++ src/stories/sample_components/fileeditor/FileEditor.jsx
@@ -32,14 +32,15 @@ const FileEditor = () => {
         const activeParent = drop.activeData.parentId;
         const overParent = drop.overData.parentId;
 
-        console.log(drop.activeData, drop.overData);
-
+        // TODO: This is a temporary solution while I switch to event based arch.
         if (activeType === "EditorTab" && overType === "EditorTabGutter") {
             if (activeParent === overParent) {
+                // Moving within same editor
                 if (overParent === parentIdRef.current) {
                     editorRef.current.moveTab(drop.activeData.node.uid, drop.overData.index);
                 }
             } else  {
+                // Moving between editors, need to remove the tab that was moved
                 if (overParent === parentIdRef.current) {
                     editorRef.current.addTab(drop.activeData.node, drop.overData.index);
                 } else if (activeParent === parentIdRef.current) {
@@ -47,17 +48,23 @@ const FileEditor = () => {
                 }
             }
         } else if (activeType === "FileTreeNode" && overType === "EditorTabGutter" && overParent === parentIdRef.current) {
+            // Moving from fileTree to editor
             editorRef.current.addTab(drop.activeData.node, drop.overData.index);
         }
     }, [drop]);
 
     useLayoutEffect(() => {
         editorRef.current.setTabGroupId(parentIdRef.current);
+        const files = [];
         flattenTree(fileTree.tree).forEach((node) => {
             if (node.type === "file") {
-                editorRef.current.addTab(node);
+                files.push(node);
             }
         });
+
+        for (let i = 0; i < 4; i++) {
+            editorRef.current.addTab(files[Math.floor(Math.random() * 3) + 1]);
+        }
     }, []);
 
     return (
