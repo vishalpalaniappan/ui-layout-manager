3137eadc20662f8959088dd625d15b4a41aaf4c2
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-23 11:23:16 -0400

    Add skeleton for menu bar component.

---
+++ src/components/LayoutManager/Components/Container/MenuBar/MenuBar.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+import "./MenuBar.scss"
+
+/**
+ * MenuBar component for the containers.
+ * 
+ * @param {Object} node
+ */
+export const MenuBar = ({node}) => {
+
+    return (
+        <></>
+    );
+}
+
+MenuBar.propTypes = {
+    content: PropTypes.object,
+}
