3ae16fd94f38371bc3ad6f8231c3a7b7a46107cd
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-26 19:32:19 -0400

    Setup modal provider and expose API.

--- src/components/LayoutManager/Providers/ModalProvider.js
+++ src/components/LayoutManager/Providers/ModalProvider.js
@@ -1,20 +1,62 @@
-import React, {createContext, useContext} from "react";
+import React, {createContext, useCallback, useContext, useMemo, useState} from "react";
+
+import {createPortal} from "react-dom";
 
 const ModalContext = createContext(null);
 
 export function ModalProvider ({children}) {
+    const [modal, setModal] = useState(null);
+
+    const openModal = useCallback((render) => {
+        const close = () => {
+            setModal(null);
+        };
+        
+        const id = `modal-${Date.now()}`;
+        setModal({id, render, close});
+
+        return {id, close};
+    }, []);
+
+    const closeModal = useCallback(() => {
+        setModal(null);
+    }, []);
+
+
+    const getPortal = () => {
+        return createPortal(
+            <>
+                {modal && (
+                    <React.Fragment key={modal.id}>
+                        {modal.render({close: modal.close})}
+                    </React.Fragment>
+                )}
+            </>,
+            document.body
+        );
+    };
+
+    const api = useMemo(() => ({
+        openModal,
+        closeModal,
+    }), [openModal, closeModal]);
 
     return (
-        <ModalContext.Provider value={{}}>
+        <ModalContext.Provider value={api}>
             {children}
+            {getPortal()}
         </ModalContext.Provider>
     );
-};
+}
 
+/**
+ * Modal manager hook to access the modal API
+ * @return {Object} The modal manager API
+ */
 export function useModalManager () {
-    const api = useContext(ModalContext);
-    if (!api) {
+    const value = useContext(ModalContext);
+    if (!value) {
         throw new Error("useModalManager must be used inside ModalProvider");
     }
-    return api;
+    return value;
 }
